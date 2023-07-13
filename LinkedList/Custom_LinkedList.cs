dff5a57e1b9c096a9c617c231b5535c394c3c99b
Harshita <chandaliyaharshita222@gmail.com>
2023-07-13 20:16:14 +0530

    Create a simple linkedlist

---
+++ LinkedList/Custom_LinkedList.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LinkedList
+{
+    public class Custom_LinkedList
+    {
+        public Node head;
+
+        public void AddLast(int data)
+        {
+            Node newNode = new Node(data);
+            if (head == null)
+            {
+                head = newNode;
+                Console.WriteLine("{0} is added to linkedlist", newNode.data);
+            }
+            else
+            {
+                Node temp = GetLastNode();
+                temp.next = newNode;
+                Console.WriteLine("{0} is added to linkedlist", newNode.data);
+            }
+        }
+
+        public void Display()
+        {
+            if (head == null)
+                Console.WriteLine("Linkedlist is empty");
+            else
+            {
+                Node temp = head;
+                Console.WriteLine("\nNodes are:");
+                while (temp != null)
+                {
+                    Console.Write(temp.data + " ");
+                    temp = temp.next;
+                }
+                Console.WriteLine("\n");
+            }
+        }
+
+        private Node GetLastNode()
+        {
+            Node temp = head;
+            while (temp.next != null)
+                temp = temp.next;
+            return temp;
+        }
+    }
+
+}
---
+++ LinkedList/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace LinkedList
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to Linked list Problem!");
+            Console.WriteLine("Program to create simple linkedlist");
+            Custom_LinkedList linkedList = new Custom_LinkedList();
+            linkedList.AddLast(56);
+            linkedList.AddLast(30);
+            linkedList.AddLast(70);
+            linkedList.Display();
+        }
+    }
+}
