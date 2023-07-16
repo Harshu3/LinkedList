c9f079563fa9d6a61f29d1295d7c3bab4ac740f9
Harshita <chandaliyaharshita222@gmail.com>
2023-07-16 18:39:10 +0530

    Remove first node from linkedlist

--- LinkedList/Program.cs
+++ LinkedList/Program.cs
@@ -7,13 +7,13 @@ namespace LinkedList
         static void Main(string[] args)
         {
             Console.WriteLine("Welcome to Linked list Problem!");
-            Console.WriteLine("Program to insert node into linkedlist");
+            Console.WriteLine("Program to remove first node from linkedlist");
             Custom_LinkedList linkedList = new Custom_LinkedList();
             linkedList.AddLast(56);
+            linkedList.AddLast(30);
             linkedList.AddLast(70);
             linkedList.Display();
-            linkedList.Insert(2, 30);
-            Console.WriteLine("After 30 node is added into linkedlist");
+            linkedList.RemoveFirst();
             linkedList.Display();
         }
     }
